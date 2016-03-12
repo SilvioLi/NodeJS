cf07abf88494df64713ce025a3d7593f8dfc7116
Silvio Li <silviolisp@gmail.com>
2016-03-12 13:17:30 -0800

    adding lecture 22

---
+++ lecture 22 - Using JSON/app.js
@@ -0,0 +1,8 @@
+var greet = require('./greet');
+
+var eng = greet.english();
+
+console.log('english' + eng);
+greet.spanish();
+greet.portuguese();
+greet.french();
---
+++ lecture 22 - Using JSON/greet/index.js
@@ -0,0 +1,11 @@
+var english = require('./english');
+var spanish = require('./spanish');
+var french = require('./french');
+var portuguese = require('./portuguese');
+
+module.exports = {
+    english: english,
+    spanish: spanish,
+    french: french,
+    portuguese: portuguese
+};
