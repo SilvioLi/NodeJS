f4431430dd5905181d7ce608919e638ecb3df663
Silvio Li <silviolisp@gmail.com>
2016-03-13 18:29:11 -0700

    git push

--- Lecture 33 - Event Emitter/app.js
+++ Lecture 33 - Event Emitter/app.js
@@ -1,1 +1,14 @@
-//starting
+var Emitter = require('./emitter');
+
+var emtr = new Emitter();
+
+emtr.on('greet', function() {
+    console.log('Somewhere, someone said hello.');
+});
+
+emtr.on('greet', function() {
+    console.log('A greeting occured!');
+});
+
+console.log('Hello!');
+emtr.emit('greet');
