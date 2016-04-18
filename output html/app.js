a5e8cceb93a960ab1b1b3135804cefcade7f5b06
Silvio Li <silviolisp@gmail.com>
2016-04-17 20:47:09 -0700

    git push

--- output html/app.js
+++ output html/app.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
-
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var html = fs.readFileSync(_dirname + '/index.htm');
-  res.end('Hello World\n');
-
-}).listen(1338, '127.0.0.1');
+http.createServer(function(req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+    var message = 'Hello world...';
+    html = html.replace('{Message}', message);
+    res.end(html);
+    
+}).listen(1337, '127.0.0.1');
